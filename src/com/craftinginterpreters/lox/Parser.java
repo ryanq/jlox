b26aae1fa6344b52a77e6547ad7ecb24a4328b46
Ryan Quattlebaum <ryan.quattlebaum@tanium.com>
2018-01-05 10:26:55 -0500

    Challenge 6.1 Comma Operator

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -23,7 +23,19 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        return series();
+    }
+
+    private Expr series() {
+        Expr expr = equality();
+
+        while (match(COMMA)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
     }
 
     private Expr equality() {
